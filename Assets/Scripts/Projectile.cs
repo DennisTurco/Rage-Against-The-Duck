00f972b8d69bb28354ac4364befae79c5bcb5773
Toghiri <pietrovaccariinfo@gmail.com>
2023-07-28 12:12:00 +0200

    feat: tilemap
    aggiunta tilemap, ordine di priorità su sprite render, modificati bullet prefab e script

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -27,7 +27,7 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Destroy(gameObject);
     }
 
     IEnumerator DeathDelay()
